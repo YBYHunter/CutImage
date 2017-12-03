2f815d8b240ddb9c3cbb2b39d769226de662a53e
yu <1229512697@qq.com>
2017-12-03 09:53:36 +0800

    整理代码

--- VIPhotoViewDemo/ViewController.m
+++ VIPhotoViewDemo/ViewController.m
@@ -26,27 +26,15 @@
     VIPhotoView *photoView = [[VIPhotoView alloc] initWithFrame:CGRectMake(10, 20, self.view.frame.size.width - 20, self.view.frame.size.width - 220) andImage:image];
     photoView.backgroundColor = [UIColor redColor];
     photoView.autoresizingMask = (1 << 6) -1;
+    [photoView centerPositionWithAnimation:NO];
     _photoView = photoView;
     [self.view addSubview:photoView];
     [self.view addSubview:self.cutImageView];
     
-    [self.photoView centerPositionWithAnimation:NO];
-    
     self.cutImageView.frame = CGRectMake((self.view.frame.size.width - 100)/2, photoView.frame.origin.y + photoView.frame.size.height + 10, 100, 100);
-    
-    UIView * lineView = [[UIView alloc] init];
-    lineView.frame = self.cutImageView.bounds;
-    lineView.backgroundColor = [UIColor clearColor];
-    lineView.layer.borderColor = [UIColor redColor].CGColor;
-    lineView.layer.borderWidth = 2;
-    [self.cutImageView addSubview:lineView];
 }
 
-- (void)viewDidLayoutSubviews
-{
-    [super viewDidLayoutSubviews];
-    NSLog(@"%@", NSStringFromCGRect([[[self.view subviews] lastObject] frame]));
-}
+#pragma mark - touch method
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     NSLog(@"touchesBegan - rect = %@",NSStringFromCGRect(_photoView.cutRect));
@@ -56,6 +44,8 @@
     
 }
 
+#pragma mark - cut image
+
 - (UIImage *)imageFromImage:(UIImage *)image inRect:(CGRect)rect {
     CGImageRef sourceImageRef = [image CGImage];
     CGImageRef newImageRef = CGImageCreateWithImageInRect(sourceImageRef, rect);
@@ -63,6 +53,8 @@
     CGImageRelease(newImageRef);
     return newImage;
 }
+
+#pragma mark - getter
 
 - (UIImageView *)cutImageView {
     if (_cutImageView == nil) {
